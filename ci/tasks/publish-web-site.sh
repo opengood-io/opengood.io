06088d2a90bb75415b262542549cf29f8ab2a9a2
Chris Jaehnen <cjaehnen@me.com>
2018-04-15 23:37:07 -0600

    Add git sync for submodules

--- ci/tasks/publish-web-site.sh
+++ ci/tasks/publish-web-site.sh
@@ -7,6 +7,11 @@ git config --global user.email "${git_email}"
 git clone web-site web-site-generated
 
 cd source-code
+
+git submodule init
+git submodule sync
+git submodule foreach "(git checkout master; git pull)"
+
 hugo
 cp -r public/* ../web-site-generated
 cp CNAME ../web-site-generated
