064a6ae85ca3df0fbb77c0ddc8b7d14e70ee9c6a
Chris Jaehnen <cjaehnen@me.com>
2019-11-22 11:57:32 -0500

    Update install script

--- bin/install-jekyll.sh
+++ bin/install-jekyll.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+bash_profile=~/.bash_profile
+
 echo -e "Downloading Ruby..." 1>&2
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo -e "Done!" 1>&2
@@ -13,9 +15,11 @@ export PATH=/usr/local/opt/ruby/bin:$PATH
 echo -e "Done!" 1>&2
 
 echo -e "Storing Ruby path to PATH env var in Bash profile and resourcing it..." 1>&2
-echo "# Ruby" >>~/.bash_profile
-echo "export PATH=/usr/local/opt/ruby/bin:\$PATH" >>~/.bash_profile
-printf '\n' >>~/.bash_profile
+cat <<EOF >>"${bash_profile}"
+# export Ruby directories in PATH env variable
+export PATH=/usr/local/opt/ruby/bin:\$PATH
+EOF
+source "${bash_profile}"
 echo -e "Done!" 1>&2
 
 echo -e "Outputting Ruby installation and version..." 1>&2
@@ -36,9 +40,11 @@ export PATH=$HOME/.gem/ruby/${ruby_version}.0/bin:$PATH
 echo -e "Done!" 1>&2
 
 echo -e "Storing Ruby Gem path to PATH env var in Bash profile and resourcing it..." 1>&2
-echo "export PATH=$HOME/.gem/ruby/${ruby_version}.0/bin:\$PATH" >> ~/.bash_profile
-printf '\n' >>~/.bash_profile
-source ~/.bash_profile
+cat <<EOF >>"${bash_profile}"
+export PATH=\$HOME/.gem/ruby/${ruby_version}.0/bin:\$PATH
+
+EOF
+source "${bash_profile}"
 echo -e "Done!" 1>&2
 
 echo -e "Outputting Ruby Gem paths..." 1>&2
